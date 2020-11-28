2795c9e30588693868b96f74acd21d950739e7c9
Michael Nnaji <mnnaji3@uwo.ca>
2020-11-28 16:34:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+ get '/' do
+   "Hello, World!"
+ end
 
+ get '/search' do
+    "Search"
+  end
 
 end
